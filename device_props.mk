6a60d6aebeba850fd8afe0623b9da13fd1c66b71
leoxsys <leoxsyspriv@gmail.com>
2021-08-13 13:02:32 +0530

    potter: Increase EXPENSIVE_RENDERING
    We are increasing value to 3 becasue in roms like DerpFest blur shutters

--- device_props.mk
+++ device_props.mk
@@ -64,7 +64,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.supports_background_blur=1 \
 persist.sys.sf.disable_blurs=1 \
-ro.sf.blurs_are_expensive=1
+ro.sf.blurs_are_expensive=3
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
